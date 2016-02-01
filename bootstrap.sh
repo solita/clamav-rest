dcd2e9407d7556766960869757a42034cf00019f
Antti Virtanen <antti.virtanen@iki.fi>
2016-02-01 15:21:54 +0200

    simplified bootstrap. No need for wait and background process

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,9 +7,8 @@ port=${2:-3310}
 echo "using clamd server: $host:$port"
 
 # start in background
-java -jar /var/clamav-rest/clamav-rest-1.0.0.jar --clamd.host=$host --clamd.port=$port & 
+java -jar /var/clamav-rest/clamav-rest-1.0.0.jar --clamd.host=$host --clamd.port=$port
 
-echo  "waiting.."
-wait
+
 
 
