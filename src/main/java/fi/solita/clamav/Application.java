436ab3274c67cfab6b03dbaea1dc631509816a5c
Tuuli Kähkönen <tuuli.kahkonen@solita.fi>
2016-11-01 14:47:43 +0200

    Timeout, MaxFileSize and MaxRequestSize can also be given as parameters.

--- src/main/java/fi/solita/clamav/Application.java
+++ src/main/java/fi/solita/clamav/Application.java
@@ -5,36 +5,46 @@ import java.util.Map;
 
 import javax.servlet.MultipartConfigElement;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-  
+
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 /**
  * Simple Spring Boot application which acts as a REST endpoint for
- * clamd server. 
+ * clamd server.
  */
 public class Application {
-  
+
+  @Value("${clamd.maxfilesize}")
+  private String maxfilesize;
+
+  @Value("${clamd.maxrequestsize}")
+  private String maxrequestsize;
+
   @Bean
   MultipartConfigElement multipartConfigElement() {
-      MultipartConfigFactory factory = new MultipartConfigFactory();
-      factory.setMaxFileSize("20000KB");
-      factory.setMaxRequestSize("20000KB");
-      return factory.createMultipartConfig();
+    MultipartConfigFactory factory = new MultipartConfigFactory();
+    factory.setMaxFileSize(maxfilesize);
+    factory.setMaxRequestSize(maxrequestsize);
+    return factory.createMultipartConfig();
   }
-  
+
   public static void main(String[] args) {
     SpringApplication app = new SpringApplication(Application.class);
     Map<String, Object> defaults = new HashMap<String, Object>();
     defaults.put("clamd.host", "192.168.50.72");
     defaults.put("clamd.port", 3310);
+    defaults.put("clamd.timeout", 500);
+    defaults.put("clamd.maxfilesize", "20000KB");
+    defaults.put("clamd.maxrequestsize", "20000KB");
     app.setDefaultProperties(defaults);
     app.run(args);
-  } 
+  }
 }
