bd3824152c94146a790aed60f15e5eb0375f50fc
David Ducatel <dducatel@trace.fr>
2017-01-26 10:40:49 +0100

    Expose configuration variable to env
    clamd.maxfilesize and clamd.maxrequestsize can now configured by environment variable

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,15 @@ set -m
 
 host=${CLAMD_HOST:-192.168.50.72}
 port=${CLAMD_PORT:-3310}
+maxfilesize=${CLAMD_MAX_FILE_SIZE:-20000KB}
+maxrequestsize=${CLAMD_MAX_REQUEST_SIZE:-20000KB}
 
 echo "using clamd server: $host:$port"
+echo "Max file size: $maxfilesize"
+echo "Max request size: $maxrequestsize"
 
 # start in background
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port
+java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$maxfilesize --clamd.maxrequestsize=$maxrequestsize
 
 
 
