6a44c47cccc46b39cbfee62ef846292b5834b478
Machiel <machiel.van.ampting@trivento.nl>
2018-12-01 22:39:33 +0100

    add MaxFileSize

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,10 @@ set -m
 
 host=${CLAMD_HOST:-192.168.50.72}
 port=${CLAMD_PORT:-3310}
+filesize=${MAXSIZE:-10GB} 
 
 echo "using clamd server: $host:$port"
 
 # start in background
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port
-
-
-
-
+#java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port
+java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize 
