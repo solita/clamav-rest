16eddf8c734ffc02d75729278dc6fb9792f6af26
Jani Rahkola <jani.rahkola@reaktor.com>
2019-02-04 13:33:32 +0200

    QosFilter in front of /scan and /scanReply

--- src/main/java/fi/solita/clamav/Application.java
+++ src/main/java/fi/solita/clamav/Application.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import javax.servlet.MultipartConfigElement;
 
+import org.eclipse.jetty.servlets.QoSFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.boot.web.filter.OrderedHiddenHttpMethodFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -28,12 +32,34 @@ public class Application {
   @Value("${clamd.maxrequestsize}")
   private String maxrequestsize;
 
+  @Value("${clamd.qos.maxrequests}")
+  private int maxrequests;
+
+  @Value("${clamd.qos.waitms}")
+  private int waitMs;
+
+  @Value("${clamd.qos.suspendms}")
+  private int suspendMs;
+
   @Bean
   MultipartConfigElement multipartConfigElement() {
     MultipartConfigFactory factory = new MultipartConfigFactory();
     factory.setMaxFileSize(maxfilesize);
     factory.setMaxRequestSize(maxrequestsize);
     return factory.createMultipartConfig();
+  }
+
+  @Bean
+  @ConditionalOnExpression("${clamd.qos.maxrequests} > 0")
+  FilterRegistrationBean qosFitlerRegistrationBean() {
+    FilterRegistrationBean registration = new FilterRegistrationBean();
+    registration.setFilter(new QoSFilter());
+    registration.addInitParameter("maxRequests", Integer.toString(maxrequests));
+    registration.addInitParameter("waitMs", Integer.toString(waitMs));
+    registration.addInitParameter("suspendMs", Integer.toString(suspendMs));
+    registration.addUrlPatterns("/scan", "/scanReply");
+    registration.setOrder(OrderedHiddenHttpMethodFilter.DEFAULT_ORDER - 1);
+    return registration;
   }
 
   public static void main(String[] args) {
@@ -44,6 +70,9 @@ public class Application {
     defaults.put("clamd.timeout", 500);
     defaults.put("clamd.maxfilesize", "20000KB");
     defaults.put("clamd.maxrequestsize", "20000KB");
+    defaults.put("clamd.qos.maxrequests", -1);
+    defaults.put("clamd.qos.waitms", 50);
+    defaults.put("clamd.qos.suspendms", -1);
     app.setDefaultProperties(defaults);
     app.run(args);
   }
