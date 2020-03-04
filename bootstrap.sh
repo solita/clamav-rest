ecf2949b60d5dab5bb8d8c16fbd0436a78f46151
Jussi-Antti Salomaa <jussi-antti.salomaa@solita.fi>
2020-03-04 10:44:29 +0200

    Fix unit in filesize parameter default value and add timeout as command line parameter

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@ set -m
 
 host=${CLAMD_HOST:-192.168.50.72}
 port=${CLAMD_PORT:-3310}
-filesize=${MAXSIZE:-10GB} 
+filesize=${MAXSIZE:-10240MB}
+timeout=${TIMEOUT:-10000}
 
 echo "using clamd server: $host:$port"
 
 # start in background
 #java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize 
+java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout
