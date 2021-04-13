ef9cb86264d1a341a41ccf227aa7b5d3142767d7
Tero Heittola <tero.heittola@solita.fi>
2021-04-13 10:04:15 +0300

    Really disable the Jetty HTTP TRACE functionality.
    Adding a servlet filter did not disable this properly since Jetty still echoed the
    passed HTTP headers back to the caller in the response body. HTTP trace enables
    attacker to steal cookies, etc, so that's why it is wise to disable it.
    TREX-7206: Näytetään sulkutyyppi rakenteen perustiedoissa (kartta/vasen palsta).

--- src/main/java/fi/solita/clamav/Application.java
+++ src/main/java/fi/solita/clamav/Application.java
@@ -5,10 +5,19 @@ import java.util.Map;
 
 import javax.servlet.MultipartConfigElement;
 
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.security.Constraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +43,66 @@ public class Application {
     factory.setMaxFileSize(maxfilesize);
     factory.setMaxRequestSize(maxrequestsize);
     return factory.createMultipartConfig();
+  }
+
+  @Bean
+  public EmbeddedServletContainerCustomizer customizer() {
+    return new EmbeddedServletContainerCustomizer() {
+
+      @Override
+      public void customize(ConfigurableEmbeddedServletContainer container) {
+        if (container instanceof JettyEmbeddedServletContainerFactory) {
+          customizeJettyToDisableHttpTrace((JettyEmbeddedServletContainerFactory) container);
+        }
+      }
+
+      /**
+       * TRACE method leads to Cross-Site Tracking (XST) problem so it is wise to disable it.
+       *
+       * Trick to disable HTTP TRACE requests. So no one has access to the "/" using "TRACE" HTTP method.
+       * <security-constraint>
+       *   <web-resource-collection>
+       *     <web-resource-name>Disable TRACE</web-resource-name>
+       *     <url-pattern>/</url-pattern>
+       *     <http-method>TRACE</http-method>
+       *   </web-resource-collection>
+       *   <auth-constraint/>
+       * </security-constraint>
+       *
+       * See e.g. https://github.com/eclipse/jetty.project/blob/jetty-9.2.10.v20150310/jetty-webapp/src/main/config/etc/webdefault.xml#L514-L531
+       */
+      private void customizeJettyToDisableHttpTrace(JettyEmbeddedServletContainerFactory jetty) {
+        jetty.addServerCustomizers(new JettyServerCustomizer() {
+          @Override
+          public void customize(Server server) {
+            Handler originalHandler = server.getHandler();
+
+            Constraint disableTraceConstraint = new Constraint();
+            disableTraceConstraint.setName("Disable TRACE");
+            disableTraceConstraint.setAuthenticate(true);
+
+            ConstraintMapping mapping = new ConstraintMapping();
+            mapping.setConstraint(disableTraceConstraint);
+            mapping.setMethod("TRACE");
+            mapping.setPathSpec("/");
+
+            Constraint omissionConstraint = new Constraint();
+            omissionConstraint.setName("Enable everything but TRACE");
+            ConstraintMapping omissionMapping = new ConstraintMapping();
+            omissionMapping.setConstraint(omissionConstraint);
+            omissionMapping.setMethodOmissions(new String [] {"TRACE"});
+            omissionMapping.setPathSpec("/");
+
+            ConstraintSecurityHandler wrappingHandler = new ConstraintSecurityHandler();
+            wrappingHandler.addConstraintMapping(mapping);
+            wrappingHandler.addConstraintMapping(omissionMapping);
+            wrappingHandler.setHandler(originalHandler);
+
+            server.setHandler(wrappingHandler);
+          }
+        });
+      }
+    };
   }
 
   public static void main(String[] args) {
