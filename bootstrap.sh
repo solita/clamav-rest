67a0d072a3c7eeb7f304edc62cbc45897a412d2c
Rafael Scoz <rafael@accurx.com>
2023-05-18 00:23:37 +0100

    use default value for timeout_shutdown

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ port=${CLAMD_PORT:-3310}
 filesize=${MAXSIZE:-10240MB}
 timeout=${TIMEOUT:-10000}
 loglevel=${LOGLEVEL:-info}
-
+timeout_shutdown=${TIMEOUT_SHUTDOWN:-30}
 echo "using clamd server: $host:$port"
 echo "loglevel: $loglevel"
 
-exec java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout --$loglevel --server.shutdown=graceful --spring.lifecycle.timeout-per-shutdown-phase=60s
+exec java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout --$loglevel --spring.lifecycle.timeout-per-shutdown-phase=$timeout_shutdown --server.shutdown=graceful
