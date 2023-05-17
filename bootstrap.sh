3dc47e709d98b6355d294e640b0f772f8e127d7b
Rafael Scoz <rafael@accurx.com>
2023-05-18 00:25:39 +0100

    lint

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,8 @@ filesize=${MAXSIZE:-10240MB}
 timeout=${TIMEOUT:-10000}
 loglevel=${LOGLEVEL:-info}
 timeout_shutdown=${TIMEOUT_SHUTDOWN:-30}
+
 echo "using clamd server: $host:$port"
 echo "loglevel: $loglevel"
 
-exec java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout --$loglevel --spring.lifecycle.timeout-per-shutdown-phase=$timeout_shutdown --server.shutdown=graceful
+exec java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host="$host" --clamd.port="$port" --clamd.maxfilesize="$filesize" --clamd.maxrequestsize="$filesize" --clamd.timeout="$timeout" --"$loglevel" --spring.lifecycle.timeout-per-shutdown-phase="$timeout_shutdown" --server.shutdown=graceful
