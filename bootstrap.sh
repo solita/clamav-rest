72fb6fda3587c4d55fd3ffbcbe1193d8b7304a32
Rafael Scoz <rafael@accurx.com>
2023-05-18 00:12:45 +0100

    use exec command to java be pid 1

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,4 +10,4 @@ loglevel=${LOGLEVEL:-debug}
 echo "using clamd server: $host:$port"
 echo "loglevel: $loglevel"
 
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout --$loglevel --server.shutdown=graceful --spring.lifecycle.timeout-per-shutdown-phase=60s
+exec java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout --$loglevel --server.shutdown=graceful --spring.lifecycle.timeout-per-shutdown-phase=60s
